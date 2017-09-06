91d9160179aa558b1d7271553b03c3bb4c223e0f
Tom Barbalet <tom@nobleape.com>
2017-09-05 22:27:42 -0700

    Change greens

--- sim/noble/gldraw.c
+++ sim/noble/gldraw.c
@@ -92,12 +92,12 @@ void gldraw_red(void)
 
 void gldraw_green(void)
 {
-    glColor3f(0.3, 1, 0.3);
+    glColor3f(0.2, 0.4, 0.2);
 }
 
 void gldraw_lightgreen(void)
 {
-    glColor3f(0.4, 1, 0.4);
+    glColor3f(0.4, 0.8, 0.4);
 }
 
 void gldraw_orange()
